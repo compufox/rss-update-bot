8c5d117bb3bd1cbf232ad35093e439fd6ba53326
ava <dev@computerfox.xyz>
2020-02-17 11:10:10 -0500

    added unix-opts flag descriptions

--- package.lisp
+++ package.lisp
@@ -23,14 +23,17 @@
 
 (define-opts
   (:name :help
+   :description "prints this help"
    :short #\h
    :long "help")
   (:name :config
+   :description "specify a config file to load"
    :short #\c
    :long "config"
    :arg-parser #'identity
    :meta-var "FILE")
   (:name :url
+   :description "specify an rss feed url to poll for updates"
    :short #\u
    :long "url"
    :arg-parser #'identity
